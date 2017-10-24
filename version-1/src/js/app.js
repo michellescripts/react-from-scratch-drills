c43a36d74d30279bd6b5229193abc43d11c5a0b6
Michelle Bergquist <caker25@gmail.com>
2017-10-24 07:44:01 -0600

    Add react

--- version-1/src/js/app.js
+++ version-1/src/js/app.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
