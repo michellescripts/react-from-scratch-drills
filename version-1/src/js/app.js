1ac3d7ea5db0387bff15e76f7f655f36a9f9d18d
Michelle Bergquist <caker25@gmail.com>
2017-10-25 13:38:01 -0600

    style

--- version-1/src/js/app.js
+++ version-1/src/js/app.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import '../css/style.css'
 
 export default class Hello extends Component {
-  render() {
+  render () {
     return (
       <div>
         Hello from react
       </div>
-    );
+    )
   }
 }
 
-render(<Hello />, document.getElementById('app'));
+render(<Hello />, document.getElementById('app'))
