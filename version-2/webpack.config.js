439906ce53c31bf75ff38e458cb3fc5641965dde
Michelle Bergquist <caker25@gmail.com>
2017-10-26 13:52:56 -0600

    v2

--- version-2/webpack.config.js
+++ version-2/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin') //  -> ADDED IN THIS STEP
+
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
   SRC: path.resolve(__dirname, 'src'),
@@ -15,9 +17,33 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html')
-    })
+    }),
+    new ExtractTextPlugin('style.bundle.css') // CSS will be extracted to this bundle file -> ADDED IN THIS STEP
   ],
-  devServer: {
-    contentBase: paths.SRC
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 }
