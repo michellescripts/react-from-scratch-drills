8a43a20e7edca12333e1d9c037b123288ad3730c
Michelle Bergquist <caker25@gmail.com>
2017-10-26 13:17:39 -0600

    Webpack v2

---
+++ version-2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js')
+}
+
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html')
+    })
+  ],
+  devServer: {
+    contentBase: paths.SRC
+  }
+}
