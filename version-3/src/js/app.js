cbad8f67a0a513d5b68bd5dd1a97047ba95ac93c
Michelle Bergquist <caker25@gmail.com>
2017-10-26 14:59:12 -0600

    v3

---
+++ version-3/src/js/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import '../css/style.css'
+import keenImage from '../assets/keen.png'
+
+export default class Hello extends Component {
+  render () {
+    return (
+      <div>
+        Hello from react
+        <img src={keenImage} alt='Commander Keen' />
+      </div>
+    )
+  }
+}
+
+render(<Hello />, document.getElementById('app'))
